846ca57fa5371398344e44293cdfda9f114359fb
omkar jadhav <okjadhav2703@gmail.com>
2023-06-27 22:01:37 +0530

    first commit

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./About.css";
+import aboutImg from "../../images/about-img.jpg";
+
+const About = () => {
+  return (
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>About</h2>
+        </div>
+
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src = {aboutImg} alt = "" />
+          </div>
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>About BookX</h2>
+            <p className='fs-17'>BookX is a online Book Hub by the concepts we have learned till now. This project  ....</p>
+            <p className='fs-17'>When it comes to searching for books, there is only one place bookx ...</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
